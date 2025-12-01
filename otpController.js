59cc79d652a339477655c37b5d4c0997ca8c996d
Marc Justin Jadaone <marcjustinjadaone@gmail.com>
2025-12-01 23:41:54 +0800

    fix gmail blocking connection

--- otpController.js
+++ otpController.js
@@ -5,11 +5,16 @@ require("dotenv").config();
 
 // Create a transporter for sending emails
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: process.env.SMTP_HOST || "smtp.gmail.com",
+    port: process.env.SMTP_PORT || 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
+    tls: {
+        rejectUnauthorized: false
+    }
 });
 
 
